9bfb6884dd4c33c7fa9f5c552b887b246a941818
Arindam Deb <112416563+Arindam4mSLG@users.noreply.github.com>
2022-08-31 14:55:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,10 @@ else:
     streamlit.text("You entered:" + fruits_asked)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_asked)
   # streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+  # write your own comment -what does the next line do? 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # write your own comment - what does this do?
+  streamlit.dataframe(fruityvice_normalized)
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
