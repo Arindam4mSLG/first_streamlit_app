655a27c4f0340be507f4c694af8e1df26a54644f
Arindam Deb <112416563+Arindam4mSLG@users.noreply.github.com>
2022-08-31 15:58:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from fruit_load_list ")
-    return my_cur.fetchall()
+    with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list ")
+      return my_cur.fetchall()
   
 
 streamlit.header("Fruityvice Fruit Advice!")
@@ -58,8 +58,8 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 streamlit.header("The fruit load list:")
-my_data_rows = get_fruit_load_list()
-# streamlit.dataframe(my_data_rows)
+#my_data_rows = get_fruit_load_list()
+#streamlit.dataframe(my_data_rows)
 
 fruits_2b_added = streamlit.text_input("Fruit Asked","")
 if fruits_2b_added is None or fruits_asked == "" :
